ecfc5a131dec5fba4c117ba22365b4161a94d997
Henry17C <hicoyago@uce.edu.ec>
2024-01-27 10:48:47 -0500

    taller27

--- src/main/java/com/example/demo/EstudianteControllerRestful.java
+++ src/main/java/com/example/demo/EstudianteControllerRestful.java
@@ -3,6 +3,22 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="estudiantes")
 public class EstudianteControllerRestful {
+
+	@Autowired
+	private IEstudianteService estudianteService;
+	//GET
+	@GetMapping(path ="/buscar")
+	public Estudiante buscar() {
+		return this.estudianteService.buscar(1);
+	}
+	//http://localhost:8080/API/v1.0Matricula/estudiantes/buscar
+	
+	@PostMapping(path="/guardar")
+	public void guardar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.guardar(estudiante);
+	}
+	//http://localhost:8080/API/v1.0Matricula/estudiantes/guardar
 
 }
