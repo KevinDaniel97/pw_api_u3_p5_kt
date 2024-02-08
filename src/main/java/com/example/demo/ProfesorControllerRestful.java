f25c46fd5e6cbf785860ebaf512585a199abf396
Kevin Toapanta <kevin.toapanta997@gmail.com>
2024-02-07 21:15:15 -0500

    tarea9

--- src/main/java/com/example/demo/ProfesorControllerRestful.java
+++ src/main/java/com/example/demo/ProfesorControllerRestful.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.modelo.Estudiante;
 import com.example.demo.repository.modelo.Profesor;
 import com.example.demo.service.IProfesorService;
 
@@ -22,26 +28,31 @@ public class ProfesorControllerRestful {
 	@Autowired
 	private IProfesorService profesorService;
 
-	@GetMapping(path ="{id}")
-	public Profesor consultar(@PathVariable Integer id) {
-		return this.profesorService.buscar(id);
+	@GetMapping(path ="{id}", produces="application/xml")
+	public ResponseEntity<Profesor> consultar(@PathVariable Integer id) {
+		Profesor prof=this.profesorService.buscar(id);
+		return ResponseEntity.status(HttpStatus.OK).body(prof);
 	}
 	@GetMapping
-	public List<Profesor> consultarTodos(@RequestParam(required = false, defaultValue = "M") String genero){
-		return this.profesorService.buscarTodos(genero);	
+	public ResponseEntity<List<Profesor>> consultarTodos(@RequestParam(required = false, defaultValue = "M") String genero){
+		List<Profesor> lista =this.profesorService.buscarTodos(genero);	
+		HttpHeaders cabeceras = new HttpHeaders();
+	      cabeceras.add("mensaje_242", "lista consultada de manera satisfactoria");
+	      cabeceras.add("mensaje info", "el sistema va estar enmantenimiento");
+	      return new ResponseEntity<>(lista, cabeceras,242);
 	}
 	
-	@PostMapping
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void guardar(@RequestBody Profesor profesor) {
 		this.profesorService.guardar(profesor);
 	}
 
-	@PutMapping(path="/{id}")
+	@PutMapping(path="/{id}", consumes =MediaType.APPLICATION_JSON_VALUE)
 	public void actualizar(@RequestBody Profesor profesor, @PathVariable Integer id) {
 		profesor.setId(id);
 		this.profesorService.actualizar(profesor);
 	}
-	@PatchMapping(path="/{id}")
+	@PatchMapping(path="/{id}", consumes =MediaType.APPLICATION_JSON_VALUE)
 	public void actualizarParcial(@RequestBody Profesor profesor, @PathVariable Integer id) {
 		this.profesorService.actualizarParcial(profesor.getApellido(), profesor.getNombre(), profesor.getId());
 	}
